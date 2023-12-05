af9210a2a7eb61d2d578b6eccd53351c532b675c
tametsi <93092155+tametsi@users.noreply.github.com>
2023-12-05 19:13:25 +0100

    Fix port config for button on STM32G031 Discovery Kit
    ST part number: STM32G0316-DISCO
    
    Co-Authored-By: niri81 <38499069+niri81@users.noreply.github.com>

--- system.c
+++ system.c
@@ -66,16 +66,14 @@ static void init_gpio(void) {
 		};
 		HAL_GPIO_Init(GPIOA, &gpio_init_struct);
 	}
-
-	__HAL_RCC_GPIOB_CLK_ENABLE();
 	{
 		GPIO_InitTypeDef gpio_init_struct = {
-				.Pin = GPIO_PIN_7,
+				.Pin = GPIO_PIN_1,
 				.Mode = GPIO_MODE_INPUT,
 				.Speed = GPIO_SPEED_FREQ_HIGH,
 				.Pull = GPIO_PULLUP,
 		};
-		HAL_GPIO_Init(GPIOB, &gpio_init_struct);
+		HAL_GPIO_Init(GPIOA, &gpio_init_struct);
 	}
 }
 
